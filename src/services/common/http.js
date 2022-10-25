b9fdd0bad11f74defc54e3b2167a05697577cbfa
alessandro <par_214@hotmail.com>
2022-10-24 19:27:37 -0500

    feat: Add Deployed backend

--- src/services/common/http.js
+++ src/services/common/http.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `${import.meta.env.VITE_API_HOST}/api/v1`,
+  baseURL: `https://constech-api.vercel.app/`,
   headers: { 'Content-type': 'application/json' },
 });
